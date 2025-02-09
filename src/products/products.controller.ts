31af53879f7e2b54fb62764a94d834b98ab85bba
Katanon Krengsomrong <66160196@go.buu.ac.th>
2025-02-09 19:41:55 +0700

    Create products.controller.ts

---
+++ src/products/products.controller.ts
@@ -0,0 +1,43 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { ProductsService } from './products.service';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+
+@Controller('products')
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+  //Create
+  @Post()
+  create(@Body() createProductDto: CreateProductDto) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return this.productsService.create(createProductDto);
+  }
+  // Read All
+  @Get()
+  findAll() {
+    return this.productsService.findAll();
+  }
+  // Read One
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.productsService.findOne(+id);
+  }
+  // Partial Update
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
+    return this.productsService.update(+id, updateProductDto);
+  }
+  // Delete
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.productsService.remove(+id);
+  }
+}
