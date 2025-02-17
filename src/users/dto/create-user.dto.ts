43b0849768eb7ae86aa8828fbf6a65daa8fc4772
Katanon Krengsomrong <66160196@go.buu.ac.th>
2025-02-17 19:10:06 +0700

    use class-validator

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,7 +1,25 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import {
+  ArrayNotEmpty,
+  IsArray,
+  IsEmail,
+  IsNotEmpty,
+  Length,
+  Min,
+} from 'class-validator';
 export class CreateUserDto {
+  @IsEmail()
+  @Length(4, 32)
   login: string;
+
+  @IsNotEmpty()
+  @Length(10, 20)
   password: string;
+  @IsArray()
+  @ArrayNotEmpty()
   roles: ('admin' | 'user')[];
+  @IsNotEmpty()
   gender: 'male' | 'female';
+  @Min(1)
   age: number;
 }
